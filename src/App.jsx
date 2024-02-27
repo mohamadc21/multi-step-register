43c6bcbc1d8f2b2fda193336c4e83e3cda8d6065
M21 <wyattmohammad1371017@gmail.com>
2024-02-27 21:59:15 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Register from "./Register"
 
 function App() {
   return (
-    <div className="bg-[#121826] flex items-center justify-center min-h-screen text-white relative overflow-hidden flex-col">
+    <div className="bg-[#121826] flex items-center justify-center min-h-screen text-white relative overflow-hidden flex-col p-4">
 
       <div className={`absolute -top-[170px] -left-48 w-[400px]`}>
         <img src={blur} className="w-full" />
